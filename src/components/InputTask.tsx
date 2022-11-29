403ba2980ca977047bb631d4eb638f9abbafb8a9
Naru-hub <1208ny0324@gmail.com>
2022-11-29 14:36:09 +0900

    InputTaskの雛形を追加

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const InputTask = () => {
-  return <div>InputTask</div>;
+  return (
+    <div className="inputField">
+      <input type="text" className="inputTitle" />
+      <button type="button" className="addButton">
+        追加
+      </button>
+    </div>
+  );
 };
 
 export default InputTask;
