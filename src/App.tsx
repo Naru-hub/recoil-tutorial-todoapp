47d57fc4902a7618ae0681b06b2ac100b1d2a018
Naru-hub <1208ny0324@gmail.com>
2022-11-29 14:27:30 +0900

    InputTask と AddTaskのコンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import "./App.css";
+import AddTask from "./components/AddTask";
+import InputTask from "./components/InputTask";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="task">
+      <InputTask />
+      <AddTask />
+    </div>
+  );
 }
 
 export default App;
