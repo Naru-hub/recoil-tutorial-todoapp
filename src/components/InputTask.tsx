10960938adaabb52f7a48d25de335dc2d6ae450b
Naru-hub <1208ny0324@gmail.com>
2022-12-01 22:19:32 +0900

    defaultで用意した値を追加ボタンをクリックした時に取ってくる処理

--- src/components/InputTask.tsx
+++ src/components/InputTask.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./InputTask.css";
+import { useRecoilValue } from "recoil";
+import { inputTitleState } from "../states/inputTitleState";
 
 const InputTask = () => {
+  const inputTitle = useRecoilValue(inputTitleState);
+
+  const handleClick = () => {
+    console.log(inputTitle);
+  };
+
   return (
     <div className="inputField">
       <input type="text" className="inputTitle" />
-      <button type="button" className="addButton">
+      <button type="button" className="addButton" onClick={handleClick}>
         追加
       </button>
     </div>
