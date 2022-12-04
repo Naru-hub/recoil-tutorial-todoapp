5d998f5c7bc6171dc8c995d845ce810ce5a55c9b
Naru-hub <1208ny0324@gmail.com>
2022-12-04 21:18:28 +0900

    addTitleをmap関数を使って出力させる

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { addTitleState, addTitleStateLength } from "../states/addTitleState";
 import "./AddTask.css";
 
 const AddTask = () => {
+  const addTitle = useRecoilValue(addTitleState);
+  const addTitleLength = useRecoilValue(addTitleStateLength);
+
   return (
     <div className="taskField">
-      <div>5個のタスクがあります</div>
+      <div>{addTitleLength}個のタスクがあります</div>
       <ul>
-        <li>はじめてのタスク</li>
+        {addTitle.map((task) => (
+          <li key={task.id}>{task.title}</li>
+        ))}
       </ul>
     </div>
   );
